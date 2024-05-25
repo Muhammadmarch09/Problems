2b4b51a6489b500b4b7340151505b3ebbe42a61f
Muhammadmarch09 <muhammadmarch@gmail.com>
2024-05-25 09:49:25 +0600

    The second problem solved

--- 1/app.js
+++ 1/app.js
@@ -11,3 +11,22 @@ function reverseString(str){
 }
 let result = reverseString("Hello")
 // console.log(result)
+
+// Write a FizzBuzz function 
+function fizzbuzz(){
+    for(let i =0;i<=100;i++){
+        if(i%3==0 & i%5 == 0){
+            console.log("FizzBuzz")
+        }
+        else if(i%3 == 0){
+            console.log("Fizz")
+        }
+        else if(i%5 == 0){
+            console.log("Buzz")
+        }
+        else{
+            console.log(i)
+        }
+    }
+}
+// fizzbuzz()
